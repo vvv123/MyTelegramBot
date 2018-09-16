c26647701d5c583d0bc7840c8ce0185627197f22
Viktoriya Ryazhska <vryazhskaya@gmail.com>
2018-09-16 19:25:26 +0300

    add bot

---
+++ src/main/java/edu/Appl.java
@@ -0,0 +1,17 @@
+package edu;
+
+import org.telegram.telegrambots.ApiContextInitializer;
+import org.telegram.telegrambots.TelegramBotsApi;
+import org.telegram.telegrambots.exceptions.TelegramApiRequestException;
+
+public class Appl {
+    public static void main(String[] args) {
+        ApiContextInitializer.init();
+        TelegramBotsApi telegramBotsApi = new TelegramBotsApi();
+        try {
+            telegramBotsApi.registerBot(new Bot());
+        } catch (TelegramApiRequestException e) {
+            e.printStackTrace();
+        }
+    }
+}
